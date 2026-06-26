39efef729c3c4a0230bd4f2ecbf3b7657e275982
srilekha-cmyk <bsrilekha75@gmail.com>
2026-06-26 20:15:17 +0530

    Add program5.py with variable and membership examples
    Initial implementation of program5.py demonstrating variable assignment and membership testing.

---
+++ program5.py
@@ -0,0 +1,18 @@
+a = [1, 2, 3]
+b = a
+c = [1, 2, 3]
+
+print(a is b)
+print(a is c)
+
+name = "Python"
+
+if "P" in name:
+    print("P is present")
+else:
+    print("P is not present")
+
+# Output:
+# True
+# False
+# P is present
