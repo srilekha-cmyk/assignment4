b0e77c4642903429a2c8b89ea00abd2f550d4210
srilekha-cmyk <bsrilekha75@gmail.com>
2026-06-26 20:08:28 +0530

    Create program1.py

---
+++ program1.py
@@ -0,0 +1,14 @@
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print("Area =", area)
+print("Perimeter =", perimeter)
+
+# Output:
+# Enter length: 10
+# Enter width: 5
+# Area = 50.0
+# Perimeter = 30.0
